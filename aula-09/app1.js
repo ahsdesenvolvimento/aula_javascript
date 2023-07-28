dcb063f90c312e2a57bed098f148776775ec6928
André Henrique Sousa <ahsdesenvolvimento@gmail.com>
2023-07-28 20:10:45 -0300

    Update app1.js
    Uma tela com um campo, onde se o número do campo for maior que 10 aparecerá um aviso de cor VERDE e se for menor aparecerá um aviso de cor VERMELHA
    O aviso é exibido na div resultadoMensagem aparecendo se o número é menor ou maior que 10 com a sua respectiva cor de fundo.
    
    No caso de o número ser igual a 10, será exibido um aviso.

--- aula-09/app1.js
+++ aula-09/app1.js
@@ -1,16 +1,26 @@
-    const buttonElement = document.getElementById('resultadoMensagem');
-    buttonElement.addEventListener('click', function(){
-       num = document.getElementById('numeroInput').value 
-       if(num == null){
-        alert("Digite um valor válido")
-       }
-       if(num>10){ 
-            buttonElement.classList.add('button-green')
-            alert('número maior que 10')
-             
-       } else {
-            buttonElement.classList.remove('button-green')
-            buttonElement.classList.add('button-red')
-            alert('número menor que 10')
-       }
-    });
+// verificar se o valor digitado é maior que 10
+
+const buttonElement = document.getElementById('checkButton');
+const resultadoMensagemElement = document.getElementById('resultadoMensagem');
+buttonElement.addEventListener('click', 
+    function() { 
+        var valor = document.getElementById('numeroInput').value; 
+
+        if (valor == 10) {
+          alert('O valor digitado é igual a 10');
+          resultadoMensagemElement.classList.remove('button-green');  
+          resultadoMensagemElement.classList.remove('button-red');
+          resultadoMensagemElement.innerHTML = '';
+          return;
+      } else if (valor > 10) {   
+          resultadoMensagemElement.classList.remove('button-red');  
+          resultadoMensagemElement.classList.add('button-green');  
+          resultadoMensagemElement.innerHTML = 'O valor  ' + valor + '  é maior que 10!'; 
+          
+      } else { 
+          resultadoMensagemElement.classList.remove('button-green');  
+          resultadoMensagemElement.classList.add('button-red');  
+          resultadoMensagemElement.innerHTML = 'O valor ' + valor + ' é menor que 10!';  
+          
+      }
+  });
