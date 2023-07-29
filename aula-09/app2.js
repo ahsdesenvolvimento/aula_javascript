10d8e33ca7d19a3a9adba508daddee0996cb706d
André Henrique Sousa <61280435+ahsdesenvolvimento@users.noreply.github.com>
2023-07-29 13:00:23 -0300

    jogo advinhação
    realizado a atualização para que após acertar o numero gerado aleatóriamente , o número de tentativas volte ao inicio e se inicie uma nova contagem.

--- aula-09/app2.js
+++ aula-09/app2.js
@@ -8,7 +8,7 @@ buttonElement.addEventListener('click',
     function() { 
         var numero = document.getElementById('tentativaInput').value; 
         
-
+        console.log(sorteado);
         if (numero < 1 || numero > 100){
             tentativas++
             resultadoMensagemElement.classList.add('result-error');
@@ -19,21 +19,27 @@ buttonElement.addEventListener('click',
             tentativas++
             resultadoMensagemElement.classList.remove('result-error');
             resultadoMensagemElement.classList.add('result-fail');
-            resultadoMensagemElement.innerHTML = '<br>'+'<strong> VOCÊ ERROU... TENTE NOVAMENTE... </strong>'+'<br><br>'+'o número sorteado é maior que o número digitado ' + numero;
+            resultadoMensagemElement.innerHTML = '<br>'+'<strong> VOCÊ ERROU... TENTE NOVAMENTE... </strong>'
+            +'<br><br>'+'o número sorteado é maior que o número digitado ' + numero;
             
 
         } else if (numero >= 1 && numero <= 100 && sorteado < numero){
             tentativas++
             resultadoMensagemElement.classList.remove('result-error');
             resultadoMensagemElement.classList.add('result-fail');
-            resultadoMensagemElement.innerHTML = '<br>'+'<strong> VOCÊ ERROU... TENTE NOVAMENTE... </strong>'+'<br><br>'+'o número sorteado  é menor que o número digitado ' + numero;
+            resultadoMensagemElement.innerHTML = '<br>'+'<strong> VOCÊ ERROU... TENTE NOVAMENTE... </strong>'+
+            '<br><br>'+'o número sorteado  é menor que o número digitado ' + numero;
          
             
         } else if (numero >= 1 && numero <= 100 && sorteado == numero){
             resultadoMensagemElement.classList.remove('result-error');
             resultadoMensagemElement.classList.remove('result-fail');
             resultadoMensagemElement.classList.add('result-success');
-            resultadoMensagemElement.innerHTML = '<br>'+'<strong> PARABÉNS ! VOCÊ ACERTOU !!! </strong>' + '<br><br>' +'o número sorteado  ' + sorteado + '  é igual ao número digitado ' + numero + '<br><br>'+'acertou na tentativa número: '+ tentativas;
+            resultadoMensagemElement.innerHTML = '<br>'+'<strong> PARABÉNS ! VOCÊ ACERTOU !!! </strong>' + 
+            '<br><br>' +'o número sorteado  ' + sorteado + '  é igual ao número digitado ' + numero + 
+            '<br><br>'+'acertou na tentativa número: '+ tentativas;
+            tentativas=1;
         
         }
+        
   });
