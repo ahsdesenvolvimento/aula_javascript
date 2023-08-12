0ca0a585f0260d162f4791b1770397395aecbee0
André Henrique Sousa <ahsdesenvolvimento@gmail.com>
2023-08-12 08:35:54 -0300

    To-Do-List com JQuery
    Aula SENAC Americana, utilizando JQuery

--- aula11.02/app04.js
+++ aula11.02/app04.js
@@ -1,30 +1,41 @@
-//$(document).ready
-//$('#addTask').click(() => {
-  //  var newTask = $('#newTask').val().trim()
-   // console.log(newTask)
-
-   // var taskItem = $("<div><div>").text(newText);
-    //taskItem.addClass("task");
-
-//})
 
 $(document).ready(function () {
-
+    //função para adicionar uma tarefa a lista
     function addTask(taskText) {
+
         var taskItem = $("<div></div>").text(taskText);
-        taskItem.addClass("task");
-        });
-    
+            taskItem.addClass("task");
+
         var doneButton = $("<button>Concluido</button>").click(function () {
-        taskItem.addClass("done");
-    
+            taskItem.addClass("done");
         });
 
-        var deleteButton = $("<button>Remover</button>").click(function () {
-        taskItem.remove();
-        
+        var deleteButton = $("<button>Remover</button>").click(function (){
+            taskItem.remove();
         });
 
         taskItem.append(doneButton, deleteButton);
-        $('#taskList").append(taskItem);
+        $("#taskList").append(taskItem);
     }
+
+    //Evento ao clicar no botão "Adicionar"
+        $("#addTask").click(function (){
+            var newTaskText = $("#newTask").val().trim();
+            if (newTaskText !=="") {
+                addTask(newTaskText);
+                $("#newTask").val("");
+            }
+        });
+
+        //Evento ao pressionar a tecla "Enter" no campo de texto
+        $("#newTask").keypress(function (event) {
+            if (event.which === 13) {
+                $("#addTask").click();
+            }
+        });
+
+        //Evento ao clicar no botão "Limpar concluidas"
+        $("#clearCompleted").click(function () {
+            $(".done").remove();
+        });
+   });
